d53ccb24e9a3a49748a980afd112102e51baceff
Serge Alhalbi <serge.alhalbi@gmail.com>
2025-07-14 23:24:34 -0400

    Add Trainer class, MRI-specific training script, and config file

---
+++ scripts/mri/train.py
@@ -0,0 +1,56 @@
+import os
+from datetime import datetime
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader, Subset
+from domains.mri.dataset import MRIClassifierDataset
+from domains.mri.model import AlzheimerMRIModel
+from training.trainer import Trainer
+from common.utils.config import load_config
+from common.utils.seed import set_seed
+
+if __name__ == "__main__":
+    # Absolute path to the root (assuming this file is scripts/mri/train.py)
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+    os.chdir(project_root)
+
+    config = load_config("configs/mri/config.yaml")
+    set_seed(config["training"]["seed"])
+    device = config["device"]
+
+    output_path = config["paths"]["model_dir"]
+    log_dir = config["paths"].get("log_dir")
+    if output_path:
+        os.makedirs(output_path, exist_ok=True)
+    if log_dir:
+        os.makedirs(log_dir, exist_ok=True)
+
+    # Setup
+    model_wrapper = AlzheimerMRIModel(
+        num_classes=config["model"]["num_classes"],
+        device=device
+    )
+    model = model_wrapper.get_model()
+
+    train_dataset = MRIClassifierDataset(split=config["dataset"]["split"])
+    train_dataset = Subset(train_dataset, range(20))  # only 20 samples
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=config["training"]["batch_size"],
+        shuffle=config["training"]["shuffle"]
+    )
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(
+        model.parameters(),
+        lr=config["training"]["learning_rate"]
+    )
+
+    trainer = Trainer(model, train_loader, criterion, optimizer, device, log_dir=log_dir)
+    trainer.train(epochs=config["training"]["epochs"])
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    model_filename = f"vit_mri_{timestamp}.pth"
+    model_path = os.path.join(output_path, model_filename)
+    torch.save(model.state_dict(), model_path)
+    print(f"Model saved to {model_path}")
